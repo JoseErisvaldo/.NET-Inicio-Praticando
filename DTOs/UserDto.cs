7e9c9b5336d70a082aaf0b3267bfeadb650b8173
joseerisvaldopg <joseerisvaldopg@gmail.com>
2025-11-16 15:57:13 -0300

    feat: adding user dto and entity

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+namespace MinhaApi.Entities;
+
+public class UserDto
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? Whatsapp { get; set; }
+}
